231a8b1b1870a4c6f66994f3f5e18e960f96844e
Abril1231 <abriln2830@gmail.com>
2024-07-05 19:48:09 -0600

    Drawin

--- lib/presentation/pages/drawingpage.dart
+++ lib/presentation/pages/drawingpage.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 import 'dart:math' as math;
 
-
-
 class DrawingPage extends StatefulWidget {
   @override
   _DrawingPageState createState() => _DrawingPageState();
 }
 
 class _DrawingPageState extends State<DrawingPage> {
+  Color uno = Color(0xFFF2E9E4);
+
   List<DrawnShape> shapes = [];
   String selectedTool = 'Pencil';
   Offset? startPosition;
@@ -18,73 +18,79 @@ class _DrawingPageState extends State<DrawingPage> {
 
   @override
   Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+
     return Scaffold(
       appBar: AppBar(
-        title: Text('Drawing App'),
-        actions: [
-          IconButton(
-            icon: Icon(Icons.text_fields),
-            onPressed: () async {
-              final text = await _showTextInputDialog(context);
-              if (text != null) {
-                setState(() {
-                  selectedTool = 'Text';
-                  textToDraw = text;
-                });
-              }
-            },
-          ),
-          IconButton(
-            icon: Icon(Icons.crop_square),
-            onPressed: () => setState(() => selectedTool = 'Square'),
-          ),
-          IconButton(
-            icon: Icon(Icons.radio_button_unchecked),
-            onPressed: () => setState(() => selectedTool = 'Circle'),
-          ),
-          IconButton(
-            icon: Icon(Icons.brush),
-            onPressed: () => setState(() => selectedTool = 'Pencil'),
-          ),
-          IconButton(
-            icon: Icon(Icons.line_weight),
-            onPressed: () => setState(() => selectedTool = 'Line'),
-          ),
-          IconButton(
-            icon: Icon(Icons.undo),
-            onPressed: () => setState(() => shapes.clear()),
-          ),
-          IconButton(
-            icon: Icon(Icons.remove_circle_outline),
-            onPressed: () => setState(() => selectedTool = 'Eraser'),
-          ),
-        ],
+        title: Text(''), backgroundColor: uno,
       ),
-      body: Column(
+      body:
+      Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
+
           Row(
+
             children: [
-              Text('Brush Size:'),
-              Slider(
-                value: brushSize,
-                min: 1.0,
-                max: 20.0,
-                onChanged: (value) {
-                  setState(() {
-                    brushSize = value;
-                  });
+              SizedBox(width: screenWidth * 0.1,
+              ),
+              IconButton(
+                icon: Icon(Icons.text_fields),
+                onPressed: () async {
+                  final text = await _showTextInputDialog(context);
+                  if (text != null) {
+                    setState(() {
+                      selectedTool = 'Text';
+                      textToDraw = text;
+                    });
+                  }
                 },
               ),
-              Text('Eraser Size:'),
-              Slider(
-                value: eraserSize,
-                min: 10.0,
-                max: 50.0,
-                onChanged: (value) {
-                  setState(() {
-                    eraserSize = value;
-                  });
-                },
+              IconButton(
+                icon: Icon(Icons.crop_square),
+                onPressed: () => setState(() => selectedTool = 'Square'),
+              ),
+              IconButton(
+                icon: Icon(Icons.radio_button_unchecked),
+                onPressed: () => setState(() => selectedTool = 'Circle'),
+              ),
+              IconButton(
+                icon: Icon(Icons.brush),
+                onPressed: () => setState(() => selectedTool = 'Pencil'),
+              ),
+              IconButton(
+                icon: Icon(Icons.line_weight),
+                onPressed: () => setState(() => selectedTool = 'Line'),
+              ),
+              IconButton(
+                icon: Icon(Icons.undo),
+                onPressed: () => setState(() {
+                  if (shapes.isNotEmpty) {
+                    shapes.removeLast();
+                  }
+                }),
+              ),
+            ],
+          ),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(width: screenWidth * 0.05,
+              ),
+              Text('Tamaño:'),
+              Expanded(
+                child: Slider(
+                  value: brushSize,
+                  min: 1.0,
+                  max: 20.0,
+                  onChanged: (value) {
+                    setState(() {
+                      brushSize = value;
+                    });
+                  },
+                ),
               ),
             ],
           ),
@@ -101,7 +107,10 @@ class _DrawingPageState extends State<DrawingPage> {
                   });
                 } else if (selectedTool == 'Eraser') {
                   _eraseShapes(details.localPosition);
-                } else if (selectedTool == 'Square' || selectedTool == 'Text' || selectedTool == 'Line' || selectedTool == 'Circle') {
+                } else if (selectedTool == 'Square' ||
+                    selectedTool == 'Text' ||
+                    selectedTool == 'Line' ||
+                    selectedTool == 'Circle') {
                   startPosition = details.localPosition;
                 }
               },
@@ -115,12 +124,15 @@ class _DrawingPageState extends State<DrawingPage> {
                 }
               },
               onPanEnd: (details) {
-                if ((selectedTool == 'Square' || selectedTool == 'Circle') && startPosition != null) {
+                if ((selectedTool == 'Square' || selectedTool == 'Circle') &&
+                    startPosition != null) {
                   setState(() {
                     shapes.add(DrawnShape(
                       start: startPosition,
                       end: details.localPosition,
-                      type: selectedTool == 'Square' ? ShapeType.square : ShapeType.circle,
+                      type: selectedTool == 'Square'
+                          ? ShapeType.square
+                          : ShapeType.circle,
                       size: brushSize,
                     ));
                     startPosition = null;
@@ -183,77 +195,43 @@ class _DrawingPageState extends State<DrawingPage> {
 
   void _eraseShapes(Offset position) {
     setState(() {
-      shapes.forEach((shape) {
+      shapes.removeWhere((shape) {
         if (shape.type == ShapeType.pencil && shape.points != null) {
-          // Modificar los puntos del lápiz para borrar donde pase
           List<Offset> remainingPoints = [];
+          bool erased = false;
           for (int i = 0; i < shape.points!.length - 1; i++) {
             Offset p1 = shape.points![i];
             Offset p2 = shape.points![i + 1];
-            if (!_isSegmentInEraser(position, p1, p2)) {
+            if (_isSegmentInEraser(position, p1, p2)) {
+              erased = true;
+            } else {
               remainingPoints.add(p1);
             }
           }
-          remainingPoints.add(shape.points!.last); // Añadir el último punto
-          shape.points = remainingPoints;
-        } else if ((shape.type == ShapeType.square || shape.type == ShapeType.circle || shape.type == ShapeType.text || shape.type == ShapeType.line) &&
-            shape.start != null && shape.end != null) {
-          // Modificar las formas para borrar donde pase el borrador
-          if (_isShapeInEraser(position, shape)) {
-            // Si la forma está completamente dentro del borrador, no hacer nada
-            return;
+          if (erased) {
+            remainingPoints.add(shape.points!.last);
+            shape.points = remainingPoints;
+            return false; // Shape not fully erased
           }
-          // Para cada tipo de forma, ajustar cómo se maneja el borrado
-          if (shape.type == ShapeType.square) {
-            // Borrar partes del cuadrado según el borrador
-            Rect shapeRect = Rect.fromPoints(shape.start!, shape.end!);
-            if (shapeRect.contains(position)) {
-              // Calcular la intersección y actualizar el cuadrado
-              Offset delta = Offset(shapeRect.center.dx - position.dx, shapeRect.center.dy - position.dy);
-              Offset newStart = shape.start! - delta;
-              Offset newEnd = shape.end! - delta;
-              shape.start = newStart;
-              shape.end = newEnd;
-            }
-          } else if (shape.type == ShapeType.circle) {
-            // Borrar partes del círculo según el borrador
-            double radius = (shape.start! - shape.end!).distance / 2;
-            Offset center = Offset(
-              (shape.start!.dx + shape.end!.dx) / 2,
-              (shape.start!.dy + shape.end!.dy) / 2,
-            );
-            if ((center - position).distance <= radius) {
-              // Calcular la intersección y actualizar el círculo
-              Offset delta = center - position;
-              Offset newStart = shape.start! - delta;
-              Offset newEnd = shape.end! - delta;
-              shape.start = newStart;
-              shape.end = newEnd;
-            }
-          } else if (shape.type == ShapeType.text) {
-            // Borrar el texto según el borrador
-            // Aquí puedes implementar una lógica específica para el borrado de texto si es necesario
-          } else if (shape.type == ShapeType.line) {
-            // Borrar partes de la línea según el borrador
-            if (_isLineInEraser(position, shape.start!, shape.end!)) {
-              // Calcular la intersección y actualizar la línea
-              Offset delta = Offset(shape.start!.dx - shape.end!.dx, shape.start!.dy - shape.end!.dy);
-              Offset newStart = shape.start! - delta;
-              Offset newEnd = shape.end! - delta;
-              shape.start = newStart;
-              shape.end = newEnd;
-            }
+        } else if ((shape.type == ShapeType.square ||
+            shape.type == ShapeType.circle ||
+            shape.type == ShapeType.text ||
+            shape.type == ShapeType.line) &&
+            shape.start != null &&
+            shape.end != null) {
+          if (_isShapeInEraser(position, shape)) {
+            return true; // Shape fully erased
           }
         }
+        return false; // Shape not affected
       });
-
-      // Eliminar las formas vacías (líneas del lápiz sin puntos)
-      shapes.removeWhere((shape) => shape.type == ShapeType.pencil && shape.points!.isEmpty);
     });
   }
 
   bool _isSegmentInEraser(Offset eraserCenter, Offset p1, Offset p2) {
-    double distance = ((p1.dx - p2.dx) * (eraserCenter.dy - p1.dy) - (p1.dy - p2.dy) * (eraserCenter.dx - p1.dx)).abs() /
+    double distance = ((p1.dx - p2.dx) * (eraserCenter.dy - p1.dy) -
+        (p1.dy - p2.dy) * (eraserCenter.dx - p1.dx))
+        .abs() /
         math.sqrt((p1.distanceSquared - p2.distanceSquared).toDouble());
     return distance < eraserSize / 2;
   }
@@ -270,7 +248,7 @@ class _DrawingPageState extends State<DrawingPage> {
       );
       return (center - eraserCenter).distance <= radius;
     } else if (shape.type == ShapeType.text) {
-      // Implementa la lógica para el borrado de texto si es necesario
+      // Implement text erasing logic if needed
       return false;
     } else if (shape.type == ShapeType.line) {
       return _isLineInEraser(eraserCenter, shape.start!, shape.end!);
@@ -278,16 +256,15 @@ class _DrawingPageState extends State<DrawingPage> {
     return false;
   }
 
-
   bool _isLineInEraser(Offset eraserCenter, Offset p1, Offset p2) {
-    double distance1 = math.sqrt(math.pow(eraserCenter.dx - p1.dx, 2) + math.pow(eraserCenter.dy - p1.dy, 2));
-    double distance2 = math.sqrt(math.pow(eraserCenter.dx - p2.dx, 2) + math.pow(eraserCenter.dy - p2.dy, 2));
-    double lineLength = math.sqrt(math.pow(p1.dx - p2.dx, 2) + math.pow(p1.dy - p2.dy, 2));
+    double distance1 = math.sqrt(math.pow(eraserCenter.dx - p1.dx, 2) +
+        math.pow(eraserCenter.dy - p1.dy, 2));
+    double distance2 = math.sqrt(math.pow(eraserCenter.dx - p2.dx, 2) +
+        math.pow(eraserCenter.dy - p2.dy, 2));
+    double lineLength = math.sqrt(math.pow(p1.dx - p2.dx, 2) +
+        math.pow(p1.dy - p2.dy, 2));
     return distance1 + distance2 <= lineLength + eraserSize;
   }
-
-
-
 }
 
 class DrawnShape {
@@ -298,7 +275,14 @@ class DrawnShape {
   ShapeType type;
   double size;
 
-  DrawnShape({this.points, this.start, this.end, this.text, required this.type, this.size = 2.0});
+  DrawnShape({
+    this.points,
+    this.start,
+    this.end,
+    this.text,
+    required this.type,
+    this.size = 2.0,
+  });
 }
 
 enum ShapeType { pencil, square, text, line, circle }
@@ -320,21 +304,30 @@ class ShapePainter extends CustomPainter {
         for (int i = 0; i < shape.points!.length - 1; i++) {
           canvas.drawLine(shape.points![i], shape.points![i + 1], paint);
         }
-      } else if (shape.type == ShapeType.square && shape.start != null && shape.end != null) {
+      } else if (shape.type == ShapeType.square &&
+          shape.start != null &&
+          shape.end != null) {
         canvas.drawRect(
           Rect.fromPoints(shape.start!, shape.end!),
           paint..style = PaintingStyle.stroke,
         );
-      } else if (shape.type == ShapeType.circle && shape.start != null && shape.end != null) {
-        double radius = (shape.start! - shape.end!).distance / 2;
+      } else if (shape.type == ShapeType.circle &&
+          shape.start != null &&
+          shape.end != null) {
+        double radius =
+            (shape.start! - shape.end!).distance / 2;
         Offset center = Offset(
           (shape.start!.dx + shape.end!.dx) / 2,
           (shape.start!.dy + shape.end!.dy) / 2,
         );
         canvas.drawCircle(center, radius, paint..style = PaintingStyle.stroke);
-      } else if (shape.type == ShapeType.line && shape.start != null && shape.end != null) {
+      } else if (shape.type == ShapeType.line &&
+          shape.start != null &&
+          shape.end != null) {
         canvas.drawLine(shape.start!, shape.end!, paint..style = PaintingStyle.stroke);
-      } else if (shape.type == ShapeType.text && shape.start != null && shape.text != null) {
+      } else if (shape.type == ShapeType.text &&
+          shape.start != null &&
+          shape.text != null) {
         final textSpan = TextSpan(
           text: shape.text,
           style: TextStyle(color: Colors.black, fontSize: shape.size),
